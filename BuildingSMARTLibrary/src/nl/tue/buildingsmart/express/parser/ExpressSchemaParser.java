471c2498f133f09803a38666790d56dd083a660e
Ruben de Laat <ruben@logic-labs.nl>
2016-01-18 09:26:28 +0100

    Added full antlr library and updated licenses

--- BuildingSMARTLibrary/src/nl/tue/buildingsmart/express/parser/ExpressSchemaParser.java
+++ BuildingSMARTLibrary/src/nl/tue/buildingsmart/express/parser/ExpressSchemaParser.java
@@ -1,7 +1,7 @@
 package nl.tue.buildingsmart.express.parser;
 
 /******************************************************************************
- * Copyright (C) 2009-2015  BIMserver.org
+ * Copyright (C) 2009-2016  BIMserver.org
  * 
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as
@@ -33,23 +33,6 @@ import org.slf4j.LoggerFactory;
 import com.google.common.base.Charsets;
 
 import antlr.CommonAST;
-
-/******************************************************************************
- * Copyright (C) 2009-2015  BIMserver.org
- * 
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Affero General Public License as
- * published by the Free Software Foundation, either version 3 of the
- * License, or (at your option) any later version.
- * 
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Affero General Public License for more details.
- * 
- * You should have received a copy of the GNU Affero General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- *****************************************************************************/
 
 import gnu.getopt.Getopt;
 import net.sourceforge.osexpress.parser.EasyParser;
